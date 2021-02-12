cad9dac7d0fc188fc9e5f1d33e70fb45bd8fb2a4
Patricia Thompson <pthom2503@gmail.com>
2021-02-12 23:57:56 +0000

    deployment

--- start.sh
+++ start.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-if [[ "x$PROD" == "x" ]]; then 
-	echo "This script is for starting in production."
-	echo "Use"
-	echo "   mix phx.server"
-	exit
-fi
-
-# TODO: Enable this script by removing the above.
-
 export SECRET_KEY_BASE=W68eso5YQOlbtvSNUR50N/HDWj6IaEhAwMR3LtzuBEQAefwYVbX84bvoTA7XtiGi
 export MIX_ENV=prod
 export PORT=4790
@@ -21,6 +12,4 @@ echo "Starting app..."
 
 _build/prod/rel/practice/bin/practice start
 
-# TODO: Add a systemd service file
-#       to start your app on system boot.
 
